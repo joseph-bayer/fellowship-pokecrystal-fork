5341169025688d5ee8edd16ed994a15ae9e4147e
Rangi <remy.oukaour+rangi42@gmail.com>
2020-02-05 22:12:08 -0500

    Remove most GSC maps, but not landmarks
    Search for "TODO" to see what needs editing as you add your own maps.

--- vc/pokecrystal11.constants.asm
+++ vc/pokecrystal11.constants.asm
@@ -11,8 +11,9 @@ ENDM
 ; [print forbid 2]
 	vc_const A_BUTTON
 ; [print forbid 3]
-	vc_const MAPGROUP_CIANWOOD
-	vc_const MAP_CIANWOOD_PHOTO_STUDIO
+	; TODO: Replace GROUP_NONE and MAP_NONE with the map that has the 'special PhotoStudio' NPC.
+	vc_const GROUP_NONE
+	vc_const MAP_NONE
 ; [print forbid 5]
 	vc_const NO_INPUT
 	vc_const B_BUTTON
